dd35bf8a03700f8ce4ef1d7a5a5b649e4616edd4
James Pharaoh <james@phsys.co.uk>
2013-04-02 11:45:32 +0200

    service information

--- lib/hq/log-monitor-server/script.rb
+++ lib/hq/log-monitor-server/script.rb
@@ -252,7 +252,7 @@ class Script
 			html << "<th>Service</th>\n"
 			html << "<th>Alerts</th>\n"
 			html << "<th>Details</th>\n"
-			html << "<th>More</th>\n"
+			html << "<th>View</th>\n"
 			html << "</tr>\n"
 
 			html << "</thead>\n"
@@ -280,8 +280,8 @@ class Script
 					),
 				]
 
-				html << "<td class=\"more\">%s</td>\n" % [
-					"<a href=\"%s\">more...</a>" % [
+				html << "<td class=\"view\">%s</td>\n" % [
+					"<a href=\"%s\">view</a>" % [
 						"/service/%s" % [
 							esc_ue(summary["service"]),
 						],
@@ -306,7 +306,101 @@ class Script
 
 	def service env, context
 
-		#summary = @db["summaries"].find({ "service
+		summaries =
+			@db["summaries"]
+				.find({
+					"_id.service" => context[:service]
+				})
+				.to_a
+
+		title =
+			"Service %s - Log monitor" % [
+				context[:service],
+			]
+
+		headers = {}
+		html = []
+
+		html << "<!DOCTYPE html>\n"
+		html << "<html>\n"
+		html << "<head>\n"
+
+		html << "<title>%s</title>\n" % [
+			esc_ht(title),
+		]
+
+		html << "</head>\n"
+		html << "<body>\n"
+
+		html << "<h1>%s</h1>\n" % [
+			esc_ht(title),
+		]
+
+		if summaries.empty?
+			html << "<p>No events have been logged for this service</p>\n"
+		else
+
+			html << "<table id=\"summaries\">\n"
+			html << "<thead>\n"
+
+			html << "<tr>\n"
+			html << "<th>Host</th>\n"
+			html << "<th>Class</th>\n"
+			html << "<th>Alerts</th>\n"
+			html << "<th>Details</th>\n"
+			html << "<th>View</th>\n"
+			html << "</tr>\n"
+
+			html << "</thead>\n"
+			html << "<tbody>\n"
+
+			summaries.each do
+				|summary|
+
+				html << "<tr class=\"summary\">\n"
+
+				html << "<td class=\"host\">%s</td>\n" % [
+					esc_ht(summary["_id"]["host"]),
+				]
+
+				html << "<td class=\"service\">%s</td>\n" % [
+					esc_ht(summary["_id"]["class"]),
+				]
+
+				html << "<td class=\"alerts\">%s</td>\n" % [
+					esc_ht(summary["combined"]["new"].to_s),
+				]
+
+				html << "<td class=\"detail\">%s</td>\n" % [
+					esc_ht(
+						summary["types"].map {
+							|type, counts|
+							"%s %s" % [ counts["new"], type ]
+						}.join ", "
+					),
+				]
+
+				html << "<td class=\"view\">%s</td>\n" % [
+					"<a href=\"%s\">view</a>" % [
+						"/host/%s" % [
+							esc_ue(summary["_id"]["host"]),
+						],
+					],
+				]
+
+				html << "</tr>\n"
+
+			end
+
+			html << "</tbody>\n"
+			html << "</table>\n"
+
+		end
+
+		html << "</body>\n"
+		html << "</html>\n"
+
+		return 200, headers, html
 
 	end
 
